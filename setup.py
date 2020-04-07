f322a98ea699251638efb1c7a230d2765484877b
ivanpastukhov <ivan.shepherd@yandex.ru>
2020-04-07 14:15:36 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='geof',
+    version='0.0.1',
+    description='geo lib',
+    url='https://github.com/ivanpastukhov/geof.git',
+    author='Ivan Pastukhov',
+    author_email='ivanpastukhoff@gmail.com',
+    license='unlicense',
+    packages=['geof'],
+    zip_safe=False
+)
