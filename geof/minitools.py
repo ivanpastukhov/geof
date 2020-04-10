614bd84a3361d8ba951300d4fe0868db978638f1
ivanpastukhov <ivan.shepherd@yandex.ru>
2020-04-10 12:08:14 +0300

    add new classes

--- geof/minitools.py
+++ geof/minitools.py
@@ -14,10 +14,21 @@ class OverpassWrapper:
     ## Распарсить датку с апишки
     # def ...
 
-class SRC_Transformer(Transformer):
-    def __init__(self, source_crs, target_crs):
-        super(SRC_Transformer, self).__init__()
-        return self.from_crs(source_crs, target_crs)
+class SRC_Transformer:
+    def __init__(self, source_crs="EPSG:4326", target_crs="EPSG:3857"):
+        """Transforms SRC.
+        # EPSG:3857 - Web Mercator, Google Web Mercator, Spherical Mercator, WGS 84 Web Mercator или WGS 84 / Pseudo-Mercator
+        # EPSG:4326 - WGS 84 -- WGS84 - World Geodetic System 1984, used in GPS
+        """
+        #TODO: дополнить другими SRC
+        if source_crs != "EPSG:4326" or target_crs != "EPSG:3857":
+            raise NotImplementedError('Only "EPSG:4326" to "EPSG:3857" conversion is supported at the moment.')
+        self.transformer = Transformer.from_crs(source_crs, target_crs)
+
+    def transform(self, lat, lon):
+        return self.transformer.transform(lat, lon)
+
+
 
 
 
