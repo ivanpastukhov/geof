8fbeeb32f31729bb4124f8ad2a9aed559bc615b1
ivanpastukhov <ivan.shepherd@yandex.ru>
2020-04-27 20:42:40 +0300

    add timeit, fix geoplot, reorganize project

--- geolib/tools/tools.py
+++ geolib/tools/tools.py
@@ -5,6 +5,7 @@ import time
 def timeit(logger):
     print('logger is called!')
     def decorator(func):
+        print('gonna decorate')
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             ts = time.time()
